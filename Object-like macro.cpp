97257be3d80fef7b105cbebc557dbb87e3eaceff
Theron <nishantkumarbagh@gmail.com>
2026-06-07 21:01:29 +0530

    Learning about using macro

---
+++ Object-like macro.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#define MY_NAME "Theron"
+//macro identifier should be in uppercase separated by underscore.
+//avoid macros with substitution text unless no visible alternatives exist.
+
+int main()
+{
+    std::cout << "My name is "<<MY_NAME<<".\n";
+
+    return 0;
+}
