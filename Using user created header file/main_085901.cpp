d6c7adc896ae808108e6890c4440977614b13fb9
Theron <nishantkumarbagh@gmail.com>
2026-06-07 21:03:02 +0530

    Learning how to include user created header file

---
+++ Using user created header file/main_085901.cpp
@@ -0,0 +1,9 @@
+#include "uinput.h"
+#include <iostream>
+
+int main()
+{
+    std::cout<<"The double of 3 is "<<H_INPUT;
+
+    return 0;
+}
---
+++ Using user created header file/uinput_085904.h
@@ -0,0 +1,8 @@
+#ifndef UINPUT_H_INCLUDED
+#define UINPUT_H_INCLUDED
+
+
+
+#endif // UINPUT_H_INCLUDED
+
+#define H_INPUT 6
